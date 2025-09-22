04fa43a5bb0d3457fa84ec420e190f1222d344ed
OHarvey <omari.harvey12@gmail.com>
2025-09-22 15:04:41 -0400

    Fixes to the overall code and fixed the runtime issue introduced with the changes

--- FoQuest/frontend/src/app/layout.tsx
+++ FoQuest/frontend/src/app/layout.tsx
@@ -1,4 +1,18 @@
+import "./globals.css";
+
 export const metadata = {
   title: "FoQuest",
   description: "FoQuest starter",
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
